1a499983b91daa091fb22399e1c3f5e8428b707e
Ivan <ivangutierz20@gmail.com>
2020-12-19 16:22:21 -0400

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,23 @@
 // Puerto
 //===================================
 process.env.PORT = process.env.PORT || 3000;
+
+//===================================
+// Entorno
+//===================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//===================================
+// Base de datos
+//===================================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'; 
+} else {
+    urlDB = 'mongodb+srv://ivan:ioSYVrSjNYGfhhbw@cluster0.vcj3i.mongodb.net/test';
+}
+
+process.env.URLDB = urlDB;
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 var bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
@@ -9,37 +11,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
-});
- 
-app.post('/usuario', function (req, res) {
+app.use( require('./routes/usuario'));
 
-    let body = req.body;
+mongoose.connect(process.env.URLDB,
+                {useNewUrlParser: true, useCreateIndex: true},
+                (err, res) =>{
+    if( err ) throw err;
 
-    if( body.nombre === undefined ) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-
+    console.log('Base de datos ONLINE');
 });
 
-app.put('/usuario/:id', function (req, res) {
 
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-  app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-});
+
+
+
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto', process.env.PORT);
 }); 
